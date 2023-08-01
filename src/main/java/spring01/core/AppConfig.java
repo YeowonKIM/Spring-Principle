4c0f93041069df932e4a014ee797ba64a78da414
YeowonKIM <kkyoo152@gmail.com>
2023-08-01 16:00:59 +0900

    Refactor : AppConfig 리펙토링 #4

--- src/main/java/spring01/core/AppConfig.java
+++ src/main/java/spring01/core/AppConfig.java
@@ -1,5 +1,6 @@
 package spring01.core;
 
+import spring01.core.discount.DiscountPolicy;
 import spring01.core.discount.FixDiscountPolicy;
 import spring01.core.member.MemberService;
 import spring01.core.member.MemberServiceImpl;
@@ -10,11 +11,19 @@ import spring01.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
