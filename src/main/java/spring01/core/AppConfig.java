7aa7f5e0308b831150a3aa54d8b4e681425a7507
YeowonKIM <kkyoo152@gmail.com>
2023-08-02 22:34:43 +0900

    Refactor : 스프링 컨테이너 사용 #5

--- src/main/java/spring01/core/AppConfig.java
+++ src/main/java/spring01/core/AppConfig.java
@@ -1,29 +1,38 @@
 package spring01.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring01.core.discount.DiscountPolicy;
 import spring01.core.discount.FixDiscountPolicy;
+import spring01.core.discount.RateDiscountPolicy;
 import spring01.core.member.MemberService;
 import spring01.core.member.MemberServiceImpl;
 import spring01.core.member.MemoryMemberRepository;
 import spring01.core.order.OrderService;
 import spring01.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    // Bean은 스프링 컨테이너에 등록해 주는 역할
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/spring01/core/MemberApp.java
+++ src/main/java/spring01/core/MemberApp.java
@@ -5,26 +5,23 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import spring01.core.member.Grade;
 import spring01.core.member.Member;
 import spring01.core.member.MemberService;
-import spring01.core.order.Order;
-import spring01.core.order.OrderService;
 
-public class OrderApp {
+public class MemberApp {
 
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-//        OrderService orderService = appConfig.orderService();
 
+        // AppConfig의 환경설정 정보를 스프링 컨테이너에서 관리해 줌 (Bean 붙은 애들0
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
-        Long memberId = 1L;
-        Member member = new Member(memberId, "Ruffy", Grade.VIP);
+        Member member = new Member(1L, "Ruffy", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
 
-        System.out.println("order = " + order);
     }
 }
