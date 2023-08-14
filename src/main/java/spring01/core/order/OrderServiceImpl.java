23d6989036a8d83c48b0797b792aa99e6776fb9d
YeowonKIM <kkyoo152@gmail.com>
2023-08-14 23:36:57 +0900

    Feat : 의존관계 주입(setter) #20

--- src/main/java/spring01/core/order/OrderServiceImpl.java
+++ src/main/java/spring01/core/order/OrderServiceImpl.java
@@ -9,12 +9,18 @@ import spring01.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private  MemberRepository memberRepository;
+    private  DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
     }
 
