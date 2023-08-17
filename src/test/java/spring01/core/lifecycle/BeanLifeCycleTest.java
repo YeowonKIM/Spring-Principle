3aa92f69ac1ba90c8aba18d61d19202b00bee238
YeowonKIM <kkyoo152@gmail.com>
2023-08-18 00:46:22 +0900

    Feat : Bean 생명주기 콜백 #26
    - 인터페이스(InitializingBean, DisposableBean) 사용과 단점

---
+++ src/test/java/spring01/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package spring01.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        // ConfigurableApplicationContext은 close 하기 위해 쓰임 (AnnotationConfigApplicationContext의 상위 클래스)
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://spring01-core.dev");
+            return networkClient;
+        }
+
+    }
+}
